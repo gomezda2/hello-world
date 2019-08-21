ff311b4b46f5f57e81ece244f33bd05ae590659d
gomezda2 <54364353+gomezda2@users.noreply.github.com>
2019-08-21 10:04:05 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,30 @@
+/***************************************************
+ * Java Program Name
+ ***************************************************
+ *Project/Program description
+ *
+ 
+ * David Gomez
+ * 8/21/2019
+ * CMSC 255 Sec 3
+ ***************************************************/
+ 
+ 
+ public class Hello{
+	 public static void printHeading() {
+		System.out.println("David Gomez");
+		System.out.println("Hello Java");
+		System.out.println("Prints Hello");
+		System.out.println("8/21/2019");
+		System.out.println("CSMC 255 Sec 3");
+		System.out.println();
+	 }
+	 public static void main(String [] args) {
+		 printHeading();
+		 System.out.println("Hello");
+	 }
+ }
+ 
+	 
+
+			
